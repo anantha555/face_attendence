c7bf18542573a7927234c9d486dcf8d0de2ca3b0
adi271001 <67856422+adi271001@users.noreply.github.com>
2020-08-27 21:44:40 +0530

    Update models.py

--- attendence_system/models.py
+++ attendence_system/models.py
@@ -8,6 +8,5 @@ class EmployeePresence(models.Model):
     present = models.BooleanField(default=True)
     Employee_work = models.ForeignKey(EmployeeWork, on_delete=models.CASCADE)
     date = models.DateField(auto_now=True)
-    status = models.BooleanField(default=False)  # when administrators empty the absents it changes to True and won't
     # appear in the least again
 # Create your models here.
